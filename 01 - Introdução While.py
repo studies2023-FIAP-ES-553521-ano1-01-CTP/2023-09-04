04dbbaadff8b93884a3ec53f60eb03f763182c4c
Rafael Cristofali <79152217+Rafafaaa@users.noreply.github.com>
2023-10-18 20:29:05 -0300

    Create 01 - Introdução While.py

---
+++ 01 - Introdução While.py
@@ -0,0 +1,61 @@
+#--> While
+#--> infinito
+# i = 0
+# while (i < 10):
+#   print('oi')
+
+#--> uma vez apenas
+# i = 0
+# while (i < 10):
+#   print('oi')
+#   i += 13
+
+#--> dez vezes
+# i = 0
+# while (i < 10):
+#   print('oi')
+#   i += 1
+
+#--> duas vezes
+# i = 0
+# while (i < 10):
+#   print('oi')
+#   i += 5
+
+'''--------------------------------------------------'''
+  
+#--> Sem while
+# frase = 'Eu'
+# palavra = input('Diga a próxima palavra: ')
+# frase = frase + ' ' + palavra
+# print(frase)
+# palavra = input('Diga a próxima palavra: ')
+# frase = frase + ' ' + palavra
+# print(frase)
+# palavra = input('Diga a próxima palavra: ')
+# frase = frase + ' ' + palavra
+# print(frase)
+
+#--> Com while
+# i = 0
+# frase = 'Eu'
+# while (i < 3):
+#   palavra = input(f'Diga a {i + 1}º palavra: ')
+#   frase = frase + ' ' + palavra
+#   print(frase)
+#   i += 1
+
+'''--------------------------------------------------'''
+
+# senha = '1234'
+# tentativa = input('Digite a senha: ')
+# while (senha != tentativa):
+#   tentativa = input('Digite a senha: ')
+# print('Acesso liberado')
+
+# senha = '1234'
+# tentativa = input('Digite a senha: ')
+# qtd = 1
+# while (senha != tentativa and qtd < 3):
+#   tentativa = input('Digite a senha: ')
+#   qtd += 1
